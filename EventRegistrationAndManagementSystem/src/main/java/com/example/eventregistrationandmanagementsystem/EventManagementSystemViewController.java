87762819650c5629ce6bfb3465bdbe4b549bcb87
alveesarker <asahanaz305@gmail.com>
2025-03-10 18:44:04 +0600

    set rapper class

--- EventRegistrationAndManagementSystem/src/main/java/com/example/eventregistrationandmanagementsystem/EventManagementSystemViewController.java
+++ EventRegistrationAndManagementSystem/src/main/java/com/example/eventregistrationandmanagementsystem/EventManagementSystemViewController.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
 
+import java.time.LocalDate;
+
 public class EventManagementSystemViewController
 {
     @javafx.fxml.FXML
@@ -11,31 +13,31 @@ public class EventManagementSystemViewController
     @javafx.fxml.FXML
     private TextField phoneNumberTextField;
     @javafx.fxml.FXML
-    private TableColumn eventTypeCol;
+    private TableColumn<Participant, String> eventTypeCol;
     @javafx.fxml.FXML
     private TextField emailTextField;
     @javafx.fxml.FXML
     private TextField nameTextField;
     @javafx.fxml.FXML
-    private TableView participantTableView;
+    private TableView<Participant> participantTableView;
     @javafx.fxml.FXML
     private Text showTotalRegNumText;
     @javafx.fxml.FXML
-    private ComboBox eventTypeComboBox;
+    private ComboBox<String> eventTypeComboBox;
     @javafx.fxml.FXML
     private CheckBox studentStatusCheckBox;
     @javafx.fxml.FXML
-    private TableColumn regDateCol;
+    private TableColumn<Participant, LocalDate> regDateCol;
     @javafx.fxml.FXML
-    private TableColumn idCol;
+    private TableColumn<Participant, String> idCol;
     @javafx.fxml.FXML
-    private TableColumn nameCol;
+    private TableColumn<Participant, String> nameCol;
     @javafx.fxml.FXML
-    private TableColumn emailCol;
+    private TableColumn<Participant,String> emailCol;
     @javafx.fxml.FXML
     private DatePicker registrationDatePicker;
     @javafx.fxml.FXML
-    private ComboBox filteredEventTypeComboBox;
+    private ComboBox<String> filteredEventTypeComboBox;
 
     @javafx.fxml.FXML
     public void initialize() {
